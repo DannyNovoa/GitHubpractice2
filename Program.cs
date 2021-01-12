4d7c9d850d64fb82866853076ab84b07e3d0f8a9
RenoNovoa <djnova25@gmail.com>
2021-01-12 18:04:50 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubpractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
