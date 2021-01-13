f27451dde78a56e5e4e439aabf2e7b9b583f4690
RenoNovoa <djnova25@gmail.com>
2021-01-13 15:28:14 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace GitHubpractice2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a sentence: ");
+
+            string sentence = Console.ReadLine();
+
+            int wordCount = CountWords(sentence);
+
+            Console.WriteLine($"We have {wordCount} in your input. ");
+
+            Console.ReadKey();
+
         }
+
+        static int CountWords(string sentences)
+        {
+            int wordCount = sentences.Split(' ').Length;
+
+
+            return wordCount;
+
+            
+        }
+        
+
     }
 }
